e966b7efe95d6bca245ce4bac4229b36229edf81
calebcochranebil <calebcochranebil33@gmail.com>
2023-09-19 10:26:24 -0700

    steps3-4-5

--- Exercise_1.6/recipe_mysql.py
+++ Exercise_1.6/recipe_mysql.py
@@ -2,3 +2,12 @@ import mysql.connector
 
 # Connect to the database
 conn = mysql.connector.connect(host="localhost", user="cf-python", passwd="password")
+
+# Create a cursor
+cursor = conn.cursor()
+
+# Create a database
+cursor.execute("CREATE DATABASE IF NOT EXISTS task_database")
+
+# Use the database
+cursor.execute("USE task_database")
