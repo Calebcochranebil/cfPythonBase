1f776f020cb5a9448a5677fa73abeaa2e05d5683
calebcochranebil <calebcochranebil33@gmail.com>
2023-09-19 10:34:10 -0700

    step-6

--- Exercise_1.6/recipe_mysql.py
+++ Exercise_1.6/recipe_mysql.py
@@ -11,3 +11,14 @@ cursor.execute("CREATE DATABASE IF NOT EXISTS task_database")
 
 # Use the database
 cursor.execute("USE task_database")
+
+# Create a table called Recipes
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS Recipes ("
+    "id INT AUTO_INCREMENT PRIMARY KEY,"
+    "name VARCHAR(50),"
+    "ingredients VARCHAR(255),"
+    "cooking_time INT,"
+    "difficulty VARCHAR(20)"
+    ")"
+)
