e375c44660c904b40fc14b6f4c6b6c18d75ad79c
calebcochranebil <calebcochranebil33@gmail.com>
2023-09-26 11:02:05 -0700

    step1.1-1.5

--- Exercise_1.7/recipe_app.py
+++ Exercise_1.7/recipe_app.py
@@ -1,2 +1,15 @@
 from sqlalchemy import create_engine, Column, Integer, String, or_
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+# Helper functions
+def is_alpha_space_or_hyphen(s):
+    return all(c.isalpha() or c.isspace() or c == "-" for c in s)
+
+
+# Create engine
+engine = create_engine("mysql://cf-python:password@localhost/task_database")
+
+# Create session
+Session = sessionmaker(bind=engine)
+session = Session()
